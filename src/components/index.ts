9366cc3eb7a53cc137b18c4cfaebcdafebcde9f2
Uriel RC <romanou@Uriels-MacBook-Air-8.local>
2026-04-07 02:10:28 -0600

    rocouv new portfolio

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as About } from "./About.astro";
+export { default as Contact } from "./Contact.astro";
+export { default as Experience } from "./Experience.astro";
+export { default as Header } from "./Header.astro";
+export { default as Home } from "./Home.astro";
+export { default as Marquee } from "./Marquee.astro";
+export { default as Projects } from "./Projects.astro";
+
